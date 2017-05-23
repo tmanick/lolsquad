98f0532dd37b3f7f687fcb000227fc467f1dcac5
Travis Manick <travis.manick@gmail.com>
2017-05-22 21:21:55 -0500

    First

---
+++ src/components/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class App extends React.Component {
+	constructor() {
+		super();
+
+		// Bind this to custom methods
+		//this.methodName = this.methodName.bind(this);
+
+		this.state = {
+			//stateObject: {}
+		};
+	}
+/*
+	componentWillMount() {
+	}
+
+	componentWillUnmount() {
+	}
+
+	componentWillUpdate(nextProps, nextState) {
+	}
+*/
+
+	render() {
+		/*
+		return (
+			<div className="class-name">
+				<ComponentName
+					passedState={this.state.stateName}
+					passedProp={this.props.params}
+				/>
+			</div>
+		);
+		*/
+		return (
+			
+		);
+	}
+}
+
+export default App;
