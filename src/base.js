8f8e9e980848e04652273d51cd13383a70e8bfb4
Travis Manick <tmanick@spydertrap.com>
2017-05-24 16:07:37 -0500

    no base, you sneaks

--- src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-	apiKey: "AIzaSyAVpqfQwSjAr9JP7VON9aCjpHSoU09luQA",
-	authDomain: "lol-squad.firebaseapp.com",
-	databaseURL: "https://lol-squad.firebaseio.com"
-});
-
-export default base;
